cbf79d216f809afbb2093a99fa1ff34aab26db17
Maksim Rakitin <mrakitin@bnl.gov>
2018-04-11 16:37:03 -0400

    Added OCM table and ECS

--- startup/10-motors.py
+++ startup/10-motors.py
@@ -22,3 +22,22 @@ class BeamStop(Device):
 BStop1 = BeamStop('XF:28ID1B-ES{BS:1-Ax:', name='BStop1')
 BStop2 = BeamStop('XF:28ID1B-ES{BS:2-Ax:', name='BStop2')
 
+# OCM table widget
+class OCMTable(Device):
+    ocm_y_upstream = Cpt(EpicsMotor, 'YU}Mtr')
+    ocm_y_downstream = Cpt(EpicsMotor, 'YD}Mtr')
+    ocm_x_table = Cpt(EpicsMotor, 'X}Mtr')
+
+OCM_table = OCMTable('XF:28ID1B-ES{OCM-Ax:', name='OCM_table')
+
+ECS_tel_guide = EpicsMotor('XF:28ID1B-ES{ECS-Ax:X}Mtr', name='ECS_tel_guide')
+
+
+class ECS(Device):
+    laser_y = Cpt(EpicsMotor, 'Lsr:1-Ax:Y}Mtr')
+    reflective_foil_x = Cpt(EpicsMotor, 'Foil:1-Ax:X}Mtr')
+    filter_wheel_1_phi = Cpt(EpicsMotor, 'Fltr:Whl1-Ax:Phi}Mtr')
+    filter_wheel_2_phi = Cpt(EpicsMotor, 'Fltr:Whl2-Ax:Phi}Mtr')
+
+ECS_laser_foil_filter = ECS('XF:28ID1B-ES{', name='ECS_laser_foil_filter')
+
