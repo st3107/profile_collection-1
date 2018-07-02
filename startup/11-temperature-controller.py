44a53e1bdfcd2ec536e18ee5979f9fed0ec10f18
Julien LHermitte <lhermitte@bnl.gov>
2018-07-02 11:40:40 -0400

    moved furncae

--- startup/11-temperature-controller.py
+++ startup/11-temperature-controller.py
@@ -4,6 +4,7 @@ from ophyd.mixins import EpicsSignalPositioner
 from ophyd import Component as C
 from ophyd import Component as Cpt
 from ophyd.device import DeviceStatus
+from ophyd import PVPositioner
 
 
 class CS700TemperatureController(PVPositioner):
@@ -114,3 +115,35 @@ class CryoStat(Device):
 # TODO: uncomment later once the device is available
 cryostream = CryoStat('XF:28ID1-ES:1{Env:01}', name='cryostream')
 
+
+# TODO : PV needs to be fixed for done signal
+# (doesn't work on ramp down)
+class LinkamFurnace(PVPositioner):
+    readback = C(EpicsSignalRO, 'RAMP:LIMIT:SET.VAL')
+    setpoint = C(EpicsSignal, 'RAMP:LIMIT:SET')
+    done = C(EpicsSignalRO, 'STATUS')
+    stop_signal = C(EpicsSignal, 'RAMP:CTRL:SET')
+    temperature = C(EpicsSignal, "TEMP")
+
+    def set(self, *args, timeout=None, **kwargs):
+        
+        return super().set(*args, timeout=timeout, **kwargs)
+
+    def trigger(self):
+        # There is nothing to do. Just report that we are done.
+        # Note: This really should not necessary to do --
+        # future changes to PVPositioner may obviate this code.
+        status = DeviceStatus(self)
+        status._finished()
+        return status
+
+# To allow for sample temperature equilibration time, increase
+# the `settle_time` parameter (units: seconds).
+linkam_furnace = LinkamFurnace('XF:28ID1-ES{LINKAM}:', name='cs700',
+                                   settle_time=0)
+linkam_furnace.done_value = 3
+linkam_furnace.stop_value = 0
+linkam_furnace.setpoint.kind = "normal"
+linkam_furnace.readback.kind = "normal"
+linkam_furnace.readback.name = 'temperature'
+linkam_furnace.setpoint.name = 'temperature_setpoint'
