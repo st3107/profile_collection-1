67fa4bc4db8ad5021a3aaae974a877a7c83a3a17
Ke Zhang <kz2249@columbia.edu>
2019-03-07 15:39:07 -0500

    MNT: Add tiff plugin, update mode and detector name

--- startup/82-blackfly.py
+++ startup/82-blackfly.py
@@ -4,9 +4,42 @@ from ophyd import SingleTrigger, ImagePlugin, HDF5Plugin
 from ophyd import AreaDetector
 
 
+class XPDDBlackFlyMode(Enum):
+        step = 1
+
+class XPDDBlackFlyTiffPlugin(TIFFPlugin, FileStoreTIFF, Device):
+    pass
+
+
 class XPDDBlackFlyDetector(SingleTrigger, AreaDetector):
     """PointGrey Black Fly detector(s) as used by 28-ID-D"""
     cam = ADComponent(PointGreyDetectorCam, "cam1:")
     image = ADComponent(ImagePlugin, "image1:")
 
-blackfly_det = XPDDBlackFlyDetector('XF:28IDD-BI{Det-BlackFly}', name="blackfly_det")
+    tiff = Cpt(XPDDBlackFlyTiffPlugin, 'TIFF1:',
+               read_attrs=[],
+               configuration_attrs=[],
+               write_path_template='Z:dex_data\\',
+               #write_path_template='Z:dex_data\\%Y\\%m\\%d\\',
+               read_path_template='/nsls2/xf28id2/dex_data/%Y/%m/%d/',
+               root='/nsls2/xf28id2/dex_data/')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._mode = XPDDBlackFlyMode.step
+
+    def stage(self):
+        self.cam.stage_sigs['image_mode'] = 'Single'
+        self.cam.stage_sigs['trigger_mode'] = 'Int. Software'
+        return super().stage()
+
+    def unstage(self):
+        try:
+            ret = super().unstage()
+        finally:
+            self._mode = XPDDBlackflyMode.step
+            return ret
+
+
+blackfly = XPDDBlackFlyDetector('XF:28IDD-BI{Det-BlackFly}', name="blackfly_det")
+blackfly.read_attrs = ['tiff']
