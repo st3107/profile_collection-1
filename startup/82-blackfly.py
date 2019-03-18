3d2cd4ef05e301c9cc0873e05aa983247ce8317c
dsprouster <sprouster@gmail.com>
2019-03-18 13:53:03 -0400

    WIP: work on getting blackfly to work

--- startup/82-blackfly.py
+++ startup/82-blackfly.py
@@ -4,10 +4,8 @@ from ophyd import SingleTrigger, ImagePlugin, HDF5Plugin
 from ophyd import AreaDetector
 
 
-class XPDDBlackFlyMode(Enum):
-    step = 1
 
-class XPDDBlackFlyTiffPlugin(TIFFPlugin, FileStoreTIFF, Device):
+class XPDDBlackFlyTiffPlugin(TIFFPlugin, FileStoreTIFFIterativeWrite, Device):
     pass
 
 
@@ -18,25 +16,17 @@ class XPDDBlackFlyDetector(SingleTrigger, AreaDetector):
     tiff = Cpt(XPDDBlackFlyTiffPlugin, 'TIFF1:',
                read_attrs=[],
                configuration_attrs=[],
-               write_path_template='Z:blackfly_data\\',
+               write_path_template='Z:\\blackfly_data\\%Y\\%m\\%d\\',
                read_path_template='/nsls2/xf28id2/blackfly_data/%Y/%m/%d/',
-               root='/nsls2/xf28id2/dex_data/')
+               root='/nsls2/xf28id2/blackfly_data/')
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self._mode = XPDDBlackFlyMode.step
 
     def stage(self):
         self.cam.stage_sigs['image_mode'] = 'Single'
-        self.cam.stage_sigs['trigger_mode'] = 'Int. Software'
+        self.cam.stage_sigs['trigger_mode'] = 'Internal'
         return super().stage()
-
-    def unstage(self):
-        try:
-            ret = super().unstage()
-        finally:
-            self._mode = XPDDBlackflyMode.step
-            return ret
 
 
 blackfly = XPDDBlackFlyDetector('XF:28IDD-BI{Det-BlackFly}', name="blackfly_det")
