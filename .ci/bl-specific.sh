2b50cbd299af791a457ca741b5697525ad47d31a
Abigail Giles <agiles@bnl.gov>
2021-03-09 12:06:16 -0500

    Changed where glbl.yml is located

--- .ci/bl-specific.sh
+++ .ci/bl-specific.sh
@@ -5,5 +5,7 @@
 conda install -y -c ${CONDA_CHANNEL_NAME} xpdacq
 
 mkdir -v -p ~/.config/acq/
+mkdir -v -p ~/user_data/config_base/yml/
+
 cp -v .ci/pdf.yml ~/.config/acq/
-cp -v .ci/glbl.yml ~/.config/acq/
+cp -v .ci/glbl.yml ~/user_data/config_base/yml/
