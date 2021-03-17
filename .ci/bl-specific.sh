182884bcd4634c6596535d61d8e3ced3195e35a0
Abigail Giles <agiles@bnl.gov>
2021-03-17 15:21:44 -0400

    Added xpd_beamline_config.yml file

--- .ci/bl-specific.sh
+++ .ci/bl-specific.sh
@@ -6,6 +6,10 @@ conda install -y -c ${CONDA_CHANNEL_NAME} xpdacq
 
 mkdir -v -p ~/.config/acq/
 mkdir -v -p ~/user_data/config_base/yml/
+sudo mkdir -v -p /mnt/data/bnl/xpdacq_special/data/xpdConfig/
+sudo chown -Rv $USER: /mnt/data/bnl/xpdacq_special/data/xpdConfig/
 
 cp -v .ci/pdf.yml ~/.config/acq/
 cp -v .ci/glbl.yml ~/user_data/config_base/yml/
+cp -v .ci/xpd_beamline_config.yml /mnt/data/bnl/xpdacq_special/data/xpdConfig/
+
