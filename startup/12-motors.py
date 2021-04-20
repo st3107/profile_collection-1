0835441ead41b1c1126d5c5ffa40f08d9e020606
st3107 <st3107@columbia.edu>
2021-04-20 11:49:31 -0400

    DEV: add more motors from mehmet

--- startup/12-motors.py
+++ startup/12-motors.py
@@ -1,7 +1,6 @@
-import ophyd
 from ophyd import (Device, Component as Cpt,
                    EpicsSignal, EpicsSignalRO, EpicsMotor)
-from nslsii.devices import TwoButtonShutter
+
 #import nslsii.devices
 
 Det_1_X = EpicsMotor('XF:28ID1B-ES{Det:1-Ax:X}Mtr', name='Det_1_X', labels=['positioners'])
@@ -75,4 +74,60 @@ ECS_An_tth = EpicsMotor('XF:28ID1B-ES{ECS-Ax:2Th2}Mtr', name='ECS_An_tth', label
 #detector for ECS - DO and MA
 ECS_det1 = EpicsSignalRO(  'XF:28IDC-BI:1{IM:1}:C4_1' ,name='ECS_det1')
 
+## Huber Stack
+mStackY = EpicsMotor('XF:28IDD-ES:2{Stg:Stack-Ax:Yfine}Mtr', name='mStackY')
+mStackX = EpicsMotor('XF:28IDD-ES:2{Stg:Stack-Ax:Xfine}Mtr', name='mStackX')
+mStackZ = EpicsMotor('XF:28IDD-ES:2{Stg:Stack-Ax:Z}Mtr', name='mStackZ')
+mRoll = EpicsMotor('XF:28IDD-ES:2{Stg:Stack-Ax:Roll}Mtr', name='mRoll')
+mPitch = EpicsMotor('XF:28IDD-ES:2{Stg:Stack-Ax:Pitch}Mtr', name='mPitch')
+mPhi = EpicsMotor('XF:28IDD-ES:2{Stg:Stack-Ax:Phi}Mtr', name='mPhi')
+mYBase = EpicsMotor('XF:28IDD-ES:2{Stg:Stack-Ax:Y}Mtr', name='mYBase')
+mXBase = EpicsMotor('XF:28IDD-ES:2{Stg:Stack-Ax:Xbase}Mtr', name='mXBase')
+
+# Dexela
+mDexelaPhi = EpicsMotor('XF:28IDD-ES:2{Stg:Stack-Ax:Htth}Mtr', name='mDexelaPhi')
+
+# Questar X
+mQuestarX = EpicsMotor('XF:28IDD-ES:2{Cam:Mnt-Ax:X}Mtr', name='mQuestarX')
+
+# Hexapods Z
+mHexapodsZ = EpicsMotor('XF:28IDD-ES:2{Det:Dexela-Ax:Z}Mtr', name='mHexapodsZ')
+
+# Beamstops
+mBeamStopY = EpicsMotor('XF:28IDD-ES:2{BS-Ax:X}Mtr', name='mBeamStopY')
+
+# Slits
+mSlitsYGap = EpicsMotor('XF:28IDD-ES:2{Slt-Ax:YGap}Mtr', name='mSlitsYGap')
+mSlitsYCtr = EpicsMotor('XF:28IDD-ES:2{Slt-Ax:YCtr}Mtr', name='mSlitsYGap')
+mSlitsXGap = EpicsMotor('XF:28IDD-ES:2{Slt-Ax:XGap}Mtr', name='mSlitsXGap')
+mSlitsXCtr = EpicsMotor('XF:28IDD-ES:2{Slt-Ax:XCtr}Mtr', name='mSlitsXGap')
+mSlitsTop = EpicsMotor('XF:28IDD-ES:2{Slt-Ax:T}Mtr', name='mSlitsTop')
+mSlitsBottom = EpicsMotor('XF:28IDD-ES:2{Slt-Ax:B}Mtr', name='mSlitsBottom')
+mSlitsOutboard = EpicsMotor('XF:28IDD-ES:2{Slt-Ax:O}Mtr', name='mSlitsOutboard')
+mSlitsInboard = EpicsMotor('XF:28IDD-ES:2{Slt-Ax:I}Mtr', name='mSlitsInboard')
+
+# Smartpod
+sSmartPodUnit = EpicsSignal('XF:28IDD-ES:2{SPod:1}Unit-SP', name='sSmartPodUnit')
+sSmartPodTrasX = EpicsSignal('XF:28IDD-ES:2{SPod:1-Ax:1}Pos-SP', name='sSmartPodTrasX')
+sSmartPodTrasY = EpicsSignal('XF:28IDD-ES:2{SPod:1-Ax:2}Pos-SP', name='sSmartPodTrasY')
+sSmartPodTrasZ = EpicsSignal('XF:28IDD-ES:2{SPod:1-Ax:3}Pos-SP', name='sSmartPodTrasZ')
+sSmartPodRotX = EpicsSignal('XF:28IDD-ES:2{SPod:1-Ax:1}Rot-SP', name='sSmartPodRotX')
+sSmartPodRotY = EpicsSignal('XF:28IDD-ES:2{SPod:1-Ax:2}Rot-SP', name='sSmartPodRotY')
+sSmartPodRotZ = EpicsSignal('XF:28IDD-ES:2{SPod:1-Ax:3}Rot-SP', name='sSmartPodRotZ')
+sSmartPodSync = EpicsSignal('XF:28IDD-ES:2{SPod:1}Sync-Cmd', name='sSmartPodSync')
+sSmartPodMove = EpicsSignal('XF:28IDD-ES:2{SPod:1}Move-Cmd', name='sSmartPodMove')
+
+# Sigray
+mSigrayX = EpicsMotor('XF:28IDD-ES:2{Stg:Sigray-Ax:X}Mtr', name='mSigrayX')
+mSigrayY = EpicsMotor('XF:28IDD-ES:2{Stg:Sigray-Ax:Y}Mtr', name='mSigrayY')
+mSigrayZ = EpicsMotor('XF:28IDD-ES:2{Stg:Sigray-Ax:Z}Mtr', name='mSigrayZ')
+mSigrayPitch = EpicsMotor('XF:28IDD-ES:2{Stg:Sigray-Ax:Pitch}Mtr', name='mSigrayPitch')
+mSigrayYaw = EpicsMotor('XF:28IDD-ES:2{Stg:Sigray-Ax:Yaw}Mtr', name='mSigrayYaw')
+
+FastShutter = EpicsMotor('XF:28IDC-ES:1{Sh2:Exp-Ax:5}Mtr', name='shctl1')
+
+pdu1 = EpicsSignal('XF:28IDD-CT{PDU:1}Sw:1-Sel', name='pdu1')
+pdu2 = EpicsSignal('XF:28IDD-CT{PDU:1}Sw:2-Sel', name='pdu2')
+pdu3 = EpicsSignal('XF:28IDD-CT{PDU:1}Sw:3-Sel', name='pdu3')
+pdu4 = EpicsSignal('XF:28IDD-CT{PDU:1}Sw:4-Sel', name='pdu4')
 
